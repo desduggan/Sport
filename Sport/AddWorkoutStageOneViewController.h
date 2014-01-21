d5a840daab88df30d2f750ca6cbddb10ada0c0bb
drduggan <duggan629@gmail.com>
2014-01-21 17:41:22 -0600

    work a little

---
+++ Sport/AddWorkoutStageOneViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddWorkoutStageOneViewController.h
+//  Sport
+//
+//  Created by Desmond Duggan on 1/21/14.
+//  Copyright (c) 2014 Desmond Duggan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddWorkoutStageOneViewController : UIViewController
+
+@end
---
+++ Sport/AddWorkoutStageOneViewController.m
@@ -0,0 +1,38 @@
+//
+//  AddWorkoutStageOneViewController.m
+//  Sport
+//
+//  Created by Desmond Duggan on 1/21/14.
+//  Copyright (c) 2014 Desmond Duggan. All rights reserved.
+//
+
+#import "AddWorkoutStageOneViewController.h"
+
+@interface AddWorkoutStageOneViewController ()
+
+@end
+
+@implementation AddWorkoutStageOneViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
